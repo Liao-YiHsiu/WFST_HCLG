6676e240909b788f357eb1a9faf1f980db32c4d1
YenChen Wu <yenchen.wu.wyc@gmail.com>
2015-06-26 06:57:09 +0800

    del

--- compose_all.sh
+++
@@ -1,8 +0,0 @@
-dir=~/local_home/nn_post
-rm -f $dir/UHL.fst;mkdir $dir/UHL.fst
-
-while read fname flen
-do
-echo $fname
-  fstcompose $dir/UH.fst/$fname.fst L.fst $dir/UHL.fst/$fname.fst
-done < $dir/seq_info
