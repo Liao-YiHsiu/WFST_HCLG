127f25883ea7ad39fb15a8ffe556f5a75854ae70
YenChen Wu <yenchen.wu.wyc@gmail.com>
2015-06-25 21:34:47 +0800

    new H

--- UH/compose_wfst.sh
+++ UH/compose_wfst.sh
@@ -8,5 +8,5 @@ while read fname flen
 do
 echo $fname
 fstcompile --isymbols=$dir/isyms.txt --osymbols=$dir/osyms.txt $dir/U.text.fst/U.$fname.fst_inv | fstarcsort --sort_type=olabel > $dir/U.fst/$fname.fst
-fstcompose $dir/H.fst $dir/U.fst/$fname.fst $dir/UH.fst/$fname.fst
+fstcompose $dir/U.fst/$fname.fst $dir/H.fst $dir/UH.fst/$fname.fst
 done < $dir/seq_info
--- UH/inverse.sh
+++ UH/inverse.sh
@@ -1,19 +1,18 @@
 dir=~/local_home/nn_post
 
-#rm -rf $dir/U.text.inv
-#mkdir $dir/U.text.inv
-
-#for file in $dir/U.text.fst/*.fst;
-#do
-#echo $file;
-#cut -d ' ' -f5 $file > score
-#$dir/U.text.fst/inverse score
-#cut -d ' ' -f1-4 $file > tmp 
-#paste tmp score > $file'_inv'
-#done
+for file in $dir/U.text.fst/*.fst;
+do
+echo $file;
+cut -d ' ' -f5 $file > score
+$dir/U.text.fst/inverse score
+cut -d ' ' -f1-4 $file > tmp 
+paste tmp score_inv > $file'_inv'
+done
 
 file=$dir/H.text.fst 
 cut -d ' ' -f5 $file > score
 $dir/U.text.fst/inverse score
 cut -d ' ' -f1-4 $file > tmp 
-paste tmp score > $file'_inv'
+paste tmp score_inv > $file'_inv'
+
+rm -f tmp score score_inv
