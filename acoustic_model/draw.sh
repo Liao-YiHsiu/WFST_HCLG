df49fcee585aebe8d597a3d9983c31cb0b7e7de1
YenChen Wu <yenchen.wu.wyc@gmail.com>
2015-08-18 08:07:53 +0800

    test

--- acoustic_model/draw.sh
+++ acoustic_model/draw.sh
@@ -7,3 +7,5 @@ cat UH.fst/$fname.fst | fstshortestpath --nshortest=1 | tee 1_best_$fname.UH | f
 #cat 1_best_fsem0_si1198.UH | fstprint --isymbols=isyms.txt --osymbols=phones_disambig.txt
 cd ~/WFST_HCLG/UH
 
+#head -n 1 $(ls) | tr -d '\n' | sed 's/==> /\n/g' | sed 's/ <==/,/g' | cut -d, -f1,3 > ../G_1_C_10000.transcription
+# test
